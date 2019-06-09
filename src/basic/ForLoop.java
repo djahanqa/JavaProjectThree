2a9f168ab1e2aca7d1ea736af71bf2b5128c4692
dilrubajahan <djahanqa@gmail.com>
2019-06-08 23:03:57 -0400

    forLoop Practice

---
+++ src/basic/ForLoop.java
@@ -0,0 +1,16 @@
+package basic;
+
+
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        int catboy = 10;
+        int awelet = 8;
+
+        for (catboy = 10; catboy < awelet; ++awelet) {
+
+            System.out.println("catboy is more powerful than awelet");
+        }
+    }
+}
