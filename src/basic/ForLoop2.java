a5668377332c146d7ee8ad39163b21a0e77cb1dc
dilrubajahan <djahanqa@gmail.com>
2019-06-08 23:28:29 -0400

    forLoop2 Practice

---
+++ src/basic/ForLoop2.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class ForLoop2 {
+    public static void main(String[] args) {
+        String MinnieMouse[] = {"100 years old", "Wlat Disney Character", "My favourite"};
+        for (String M : MinnieMouse) {
+            System.out.println(" Minnie Mouse " + M );
+
+        }
+    }
+}
